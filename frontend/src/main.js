5caf5a08457dd619d1045e48a23a6d1104587390
Yanick Assignon <assignon>
2021-01-20 18:03:55 +0100

    remove vuetify import from app

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -12,7 +12,6 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
-import vuetify from "./plugins/vuetify";
 import VAnimateCss from "v-animate-css";
 import Vuex from "vuex";
 import Axios from "axios";
@@ -36,6 +35,5 @@ new Vue({
   },
   router,
   store,
-  vuetify,
   render: (h) => h(App),
 }).$mount("#app");
